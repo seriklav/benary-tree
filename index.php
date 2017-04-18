0722a2ab52039e6d9ea3514b79917549984966da
Андрей Лавринюк <splaandrey@gmail.com>
2017-04-18 16:10:14 +0300

    fix file.min.css

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 //    spl_autoload_register();
 include"BinaryTree.php";
-//include_once "BinaryNode.php.php";
     function debug($obj)
     {
         echo '<pre>';
@@ -17,7 +16,7 @@ include"BinaryTree.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.min.css">
     <script src="js/script.js"></script>
 </head>
 <body>
